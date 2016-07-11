75569b00813d46d3c3c8ba8c41563248bb35c1aa
Jason Voegele <jason.voegele@gmail.com>
2016-07-11 16:49:42 -0400

    Add validation and use "limits" instead of "load map"

--- src/riak_core_throttle.erl
+++ src/riak_core_throttle.erl
@@ -25,16 +25,22 @@
 %%
 %% The throttle for an activity can be set to a specific value with the
 %% `set_throttle/2' function, or it can be set to a value based on the current
-%% "load" for the activity by using the `set_load_map/2' function to define a
+%% "load" for the activity by using the `set_limits/2' function to define a
 %% mapping from load factors to throttle values, and then periodically calling
-%% the `set_throttle_by_load/2' function with the current load factor.
+%% the `set_throttle_by_load/2' function with the current load factor. Note
+%% that "load factor" is an abstract concept, the purpose of which is simply
+%% to represent numerically the amount of load that an activity is under and
+%% map that level of load to a particular throttle level. Examples of metrics
+%% that could be used for load factors include the size of the mailbox for a
+%% particular process, or the number of messages per second being sent to an
+%% external service such as Solr.
 -module(riak_core_throttle).
 
 %% API
 -export([set_throttle/2,
          clear_throttle/1,
-         set_load_map/2,
-         clear_load_map/1,
+         set_limits/2,
+         clear_limits/1,
          set_throttle_by_load/2,
          throttle/1]).
 
@@ -48,13 +54,13 @@
 -type activity_key() :: atom().
 -type throttle_time() :: non_neg_integer().
 -type load_factor() :: integer().
--type load_map() :: [{load_factor(), throttle_time()}, ...].
+-type limits() :: [{load_factor(), throttle_time()}, ...].
 
--export_type([activity_key/0, throttle_time/0, load_factor/0, load_map/0]).
+-export_type([activity_key/0, throttle_time/0, load_factor/0, limits/0]).
 
 -define(THROTTLE_KEY(Key), list_to_atom("throttle_" ++ atom_to_list(Key))).
--define(THROTTLE_LOAD_MAP_KEY(Key),
-        list_to_atom("throttle_load_map" ++ atom_to_list(Key))).
+-define(THROTTLE_LIMITS_KEY(Key),
+        list_to_atom("throttle_limits" ++ atom_to_list(Key))).
 
 %% @doc Sets the throttle for the activity identified by `Key' to the specified
 %% `Time'.
@@ -67,37 +73,45 @@ set_throttle(Key, Time) when Time >= 0 ->
 clear_throttle(Key) ->
     application:unset_env(riak_core, ?THROTTLE_KEY(Key)).
 
-%% @doc Sets the load map for the activity identified by `Key'. The load map is
+%% @doc <p>Sets the limits for the activity identified by `Key'. `Limits' is
 %% a mapping from load factors to throttle values. Once this mapping has been
 %% established, the `set_throttle_by_load/2' function can be used to set the
-%% throttle for the activity based on the current load factor.
+%% throttle for the activity based on the current load factor.</p>
+%%
+%% <p>All of the throttle values in the `Limits' list must be non-negative
+%% integers. Additionally, the `Limits' list must contain a tuple with the
+%% key `-1'. If either of these conditions does not hold, this function exits
+%% with an error.</p>
+%% @end
+%%
 %% @see set_throttle_by_load/2
--spec set_load_map(activity_key(), load_map()) -> ok.
-set_load_map(Key, LoadMap) ->
+-spec set_limits(activity_key(), limits()) -> ok | no_return().
+set_limits(Key, Limits) ->
+    ok = validate_limits(Key, Limits),
     application:set_env(riak_core,
-                        ?THROTTLE_LOAD_MAP_KEY(Key),
-                        lists:sort(LoadMap)).
+                        ?THROTTLE_LIMITS_KEY(Key),
+                        lists:sort(Limits)).
 
-%% @doc Clears the load map for the activity identified by `Key'.
--spec clear_load_map(activity_key()) -> ok.
-clear_load_map(Key) ->
-    application:unset_env(riak_core, ?THROTTLE_LOAD_MAP_KEY(Key)).
+%% @doc Clears the limits for the activity identified by `Key'.
+-spec clear_limits(activity_key()) -> ok.
+clear_limits(Key) ->
+    application:unset_env(riak_core, ?THROTTLE_LIMITS_KEY(Key)).
 
 %% @doc Sets the throttle for the activity identified by `Key' to a value
-%% determined by consulting the load map for the activity. The throttle value
+%% determined by consulting the limits for the activity. The throttle value
 %% is the value associated with the largest load factor <= `LoadFactor'.
-%% If there is no load map defined for the activity, exits with
-%% error({no_load_map, Key}).
-%% @see set_load_map/2
+%% If there are no limits defined for the activity, exits with
+%% error({no_limits, Key}).
+%% @see set_limits/2
 -spec set_throttle_by_load(activity_key(), load_factor()) ->
     throttle_time().
 set_throttle_by_load(Key, LoadFactor) when is_integer(LoadFactor) ->
     case get_throttle_for_load(Key, LoadFactor) of
         undefined ->
-            error({no_load_map, Key});
-        ThrottleTime ->
-            set_throttle(Key, ThrottleTime),
-            ThrottleTime
+            error({no_limits, Key});
+        ThrottleVal ->
+            set_throttle(Key, ThrottleVal),
+            ThrottleVal
     end.
 
 %% @doc Sleep for the number of milliseconds specified as the throttle time for
@@ -120,25 +134,61 @@ get_throttle(Key) ->
 -spec get_throttle_for_load(activity_key(), load_factor()) ->
     throttle_time() | undefined.
 get_throttle_for_load(Key, LoadFactor) ->
-    case application:get_env(riak_core, ?THROTTLE_LOAD_MAP_KEY(Key)) of
-        {ok, LoadMap} ->
-            find_throttle_for_load_factor(LoadMap, LoadFactor);
+    case application:get_env(riak_core, ?THROTTLE_LIMITS_KEY(Key)) of
+        {ok, Limits} ->
+            find_throttle_for_load_factor(Limits, LoadFactor);
         _ ->
             undefined
     end.
 
 find_throttle_for_load_factor([], _LoadFactor) ->
     undefined;
-find_throttle_for_load_factor(LoadMap, LoadFactor) ->
+find_throttle_for_load_factor(Limits, LoadFactor) ->
     Candidates = lists:takewhile(
         fun({Load, _Time}) ->
             LoadFactor >= Load
         end,
-        LoadMap),
-    {_Load, ThrottleTime} = case Candidates of
+        Limits),
+    {_Load, ThrottleVal} = case Candidates of
         [] ->
-            lists:nth(1, LoadMap);
+            lists:nth(1, Limits);
         _ ->
             lists:last(Candidates)
     end,
-    ThrottleTime.
+    ThrottleVal.
+
+validate_limits(Key, Limits) ->
+    Validators = [validate_all_non_negative(Limits),
+                  validate_has_negative_one_key(Limits)],
+    Errors = [Message || {error, Message} <- Validators],
+    case Errors of
+        [] ->
+            ok;
+        Messages ->
+            lager:error("***** Invalid throttle limits for ~p: ~p.",
+                        [Key, Messages]),
+            error(invalid_throttle_limits)
+    end.
+
+validate_all_non_negative(Limits) ->
+    Good = lists:all(
+      fun({LoadFactor, ThrottleVal}) ->
+              is_integer(LoadFactor) andalso
+              is_integer(ThrottleVal) andalso
+              ThrottleVal >= 0
+      end,
+      Limits),
+    case Good of
+        true ->
+            ok;
+        false ->
+            {error, "All throttle values must be non-negative integers"}
+    end.
+
+validate_has_negative_one_key(Limits) ->
+    case lists:keyfind(-1, 1, Limits) of
+        {-1, _} ->
+            ok;
+        false ->
+            {error, "Must include -1 entry"}
+    end.
--- test/riak_core_throttle_tests.erl
+++ test/riak_core_throttle_tests.erl
@@ -25,7 +25,7 @@ activity_keys() ->
 clear_throttles(ActivityKeys) ->
     lists:foreach(fun(Key) ->
                           riak_core_throttle:clear_throttle(Key),
-                          riak_core_throttle:clear_load_map(Key)
+                          riak_core_throttle:clear_limits(Key)
                   end,
                   ActivityKeys).
 
@@ -35,11 +35,11 @@ throttle_test_() ->
      fun clear_throttles/1,
      [fun test_throttle_badkey/1,
       fun test_set_throttle/1,
-      fun test_set_throttle_by_load_with_no_load_map/1,
-      fun test_set_throttle_by_load_with_empty_load_map/1,
-      fun test_set_throttle_by_load_with_good_load_map/1,
+      fun test_set_throttle_by_load_with_no_limits/1,
+      fun test_set_throttle_by_load_with_good_limits/1,
       fun test_set_throttle_by_load_actually_sets_throttle/1,
-      fun test_set_load_map_does_not_overwrite_current_throttle/1
+      fun test_set_limits_does_not_overwrite_current_throttle/1,
+      fun test_set_limits_with_invalid_limits/1
      ]}.
 
 test_throttle_badkey([Key|_]) ->
@@ -50,18 +50,13 @@ test_set_throttle([Key1, Key2|_]) ->
     [?_assertEqual(42, riak_core_throttle:throttle(Key1)),
      ?_assertError({badkey, Key2}, riak_core_throttle:throttle(Key2))].
 
-test_set_throttle_by_load_with_no_load_map([Key|_]) ->
-    [?_assertError({no_load_map, Key},
+test_set_throttle_by_load_with_no_limits([Key|_]) ->
+    [?_assertError({no_limits, Key},
                    riak_core_throttle:set_throttle_by_load(Key, 50))].
 
-test_set_throttle_by_load_with_empty_load_map([Key|_]) ->
-    ok = riak_core_throttle:set_load_map(Key, []),
-    [?_assertError({no_load_map, Key},
-                   riak_core_throttle:set_throttle_by_load(Key, 50))].
-
-test_set_throttle_by_load_with_good_load_map([Key, Key2|_]) ->
-    LoadMap = [{-1, 0}, {10, 5}, {100, 10}],
-    ok = riak_core_throttle:set_load_map(Key, LoadMap),
+test_set_throttle_by_load_with_good_limits([Key, Key2|_]) ->
+    Limits = [{-1, 0}, {10, 5}, {100, 10}],
+    ok = riak_core_throttle:set_limits(Key, Limits),
     [
      ?_assertEqual(0, riak_core_throttle:set_throttle_by_load(Key, -10000000)),
      ?_assertEqual(0, riak_core_throttle:set_throttle_by_load(Key, -10)),
@@ -76,17 +71,23 @@ test_set_throttle_by_load_with_good_load_map([Key, Key2|_]) ->
      ?_assertEqual(10, riak_core_throttle:set_throttle_by_load(Key, 100)),
      ?_assertEqual(10, riak_core_throttle:set_throttle_by_load(Key, 101)),
      ?_assertEqual(10, riak_core_throttle:set_throttle_by_load(Key, 10000001)),
-     ?_assertError({no_load_map, Key2},
+     ?_assertError({no_limits, Key2},
                    riak_core_throttle:set_throttle_by_load(Key2, 100))
     ].
 
 test_set_throttle_by_load_actually_sets_throttle([Key|_]) ->
-    ok = riak_core_throttle:set_load_map(Key, [{0, 0}, {5, 42}]),
+    ok = riak_core_throttle:set_limits(Key, [{-1, 0}, {5, 42}]),
     42 = riak_core_throttle:set_throttle_by_load(Key, 5),
     [?_assertEqual(42, riak_core_throttle:throttle(Key))].
 
-test_set_load_map_does_not_overwrite_current_throttle([Key|_]) ->
-    ok = riak_core_throttle:set_load_map(Key, [{0, 0}, {100, 42}]),
+test_set_limits_does_not_overwrite_current_throttle([Key|_]) ->
+    ok = riak_core_throttle:set_limits(Key, [{-1, 0}, {100, 42}]),
     42 = riak_core_throttle:set_throttle_by_load(Key, 500),
-    ok = riak_core_throttle:set_load_map(Key, [{0, 0}, {100, 500}]),
+    ok = riak_core_throttle:set_limits(Key, [{-1, 0}, {100, 500}]),
     [?_assertEqual(42, riak_core_throttle:throttle(Key))].
+
+test_set_limits_with_invalid_limits([Key|_]) ->
+    [?_assertError(invalid_throttle_limits,
+                   riak_core_throttle:set_limits(Key, [{-1, 0}, {100, -1}])),
+     ?_assertError(invalid_throttle_limits,
+                   riak_core_throttle:set_limits(Key, [{1, 0}, {100, 100}]))].
